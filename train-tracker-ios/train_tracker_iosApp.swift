7ee9ab4aad8768dbbc7c1699d690b07fa2f7027d
Drew Rautenberg <drew@drewrautenberg.com>
2024-06-19 06:51:37 -0500

    Initial Commit

---
+++ train-tracker-ios/train_tracker_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  train_tracker_iosApp.swift
+//  train-tracker-ios
+//
+//  Created by Drew Rautenberg on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct train_tracker_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
