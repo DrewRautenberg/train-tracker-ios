4f5fec2a9d80df431d095bd7fcd73af29e3a0ac1
Drew Rautenberg <drew@drewrautenberg.com>
2024-08-02 10:41:22 -0500

    changed url to a var and file name

--- train-tracker-ios/Models/apiCall.swift
+++ train-tracker-ios/Models/apiCall.swift
@@ -1,5 +1,5 @@
 //
-//  apiStations.swift
+//  apiCall.swift
 //  train-tracker-ios
 //
 //  Created by Drew Rautenberg on 8/2/24.
@@ -7,13 +7,15 @@
 
 import Foundation
 
+let apiURL = "http://100.75.49.124"
+
 struct Stations : Decodable{
     let Station_Name: String
     let Station_Code: String
 }
 
 func fetchStations(line: String) async throws -> [Stations]{
-    let url = URL(string: "http://100.75.49.124/stations/\(line)")!
+    let url = URL(string: "\(apiURL)/stations/\(line)")!
     let (data, _) = try await URLSession.shared.data(from: url)
     let decoded = try JSONDecoder().decode([Stations].self, from: data)
     return decoded
